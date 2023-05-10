bd64d8024054e993972a1b3592def401331483b4
Dario Cruz <rio2012@gmail.com>
2023-05-10 17:02:27 -0400

    Add code.js and start of code.

---
+++ code.js
@@ -0,0 +1,10 @@
+// Create the Fibonacci Sequence with takes a given number and adds all numbers from 1 to that number together. 
+
+const iterationFib = () => {
+
+}
+
+
+const recursionFib = () => {
+    
+}
