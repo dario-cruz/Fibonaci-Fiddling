f2d7372e551402e30cc4ce524f2045a403d7ef3c
Dario Cruz <rio2012@gmail.com>
2023-05-12 13:37:54 -0400

    Change iterationFib func, now completed the sequence correctly.

--- code.js
+++ code.js
@@ -1,11 +1,19 @@
 // Create the Fibonacci Sequence with takes a given givenNumber and adds all givenNumbers from 1 to that givenNumber together. 
 
 const iterationFib = (givenNumber) => {
-    let currentNum = 0
-    do {
-        currentNum += currentNum + 1
-        console.log(currentNum)
-    } while (currentNum < givenNumber)
+    let numA = 0, numB = 1, nextNum
+    // Console.log the first number in the Fibonacci Sequence.
+    console.log(numA)
+
+    while (numB <= givenNumber) {
+        // Log the current number.
+        console.log(numB)
+
+        // Add the values to get the next number in the sequence
+        nextNum = numA + numB
+        numA = numB
+        numB = nextNum
+    }
 }
 
 
@@ -13,6 +21,6 @@ const recursionFib = (givenNumber) => {
     if (givenNumber == 0) {
         return givenNumber
     } else {
-        return recursionFib(givenNumber) + givenNumber++
+        return recursionFib(givenNumber + givenNumber)
     }
 }
