95cf001aa3b89cbfd8ad2dda6f83c5cd1ce1cf5a
Dario Cruz <rio2012@gmail.com>
2023-05-15 16:23:14 -0400

    Change recursion and iteration funcs all code works. Add comments to explain.

--- code.js
+++ code.js
@@ -7,26 +7,41 @@
 
 
 const iterationFib = (givenNumber) => {
-    let numA = 0, numB = 1, nextNum
-    // Console.log the first number in the Fibonacci Sequence.
-    console.log(numA)
+    // Create the array that will hold the end result.
+    let fibArray = []
 
-    while (numB <= givenNumber) {
-        // Log the current number.
-        console.log(numB)
-
-        // Add the values to get the next number in the sequence
-        nextNum = numA + numB
-        numA = numB
-        numB = nextNum
+    // Iterate through the decreasing value of giveNumber
+    for(let i = 0; i < givenNumber; i++) {
+        // if the value is less then 1 just add it to the array.
+        if (i <= 1) {
+            fibArray.push(i)
+        } else {
+            // Else calculate the sequence using formula. 
+            let num = fibArray[i - 1] + fibArray[i - 2]
+            // Add the number to the array to create the sequence.
+            fibArray.push(num)
+        }
     }
+    return fibArray    
 }
 
 
 const recursionFib = (givenNumber) => {
-    if (givenNumber == 0) {
-        return givenNumber
+    if (givenNumber <= 0) {
+        // If number is negative do nothing, ie return empty array. 
+        return []
+    } else if (givenNumber === 1) {
+        // If number is 1 then just return array of 0.
+        return [0]
+    } else if (givenNumber === 2) {
+        // If number is 2 then add 0,1 to array due to fib calc.
+        return [0,1]
     } else {
-        return recursionFib(givenNumber + givenNumber)
+        const fibArray = recursionFib(givenNumber - 1)
+        const firstNum = fibArray[fibArray.length - 1]
+        const secondNum = fibArray[fibArray.length - 2]
+        const currentNum = firstNum + secondNum
+        fibArray.push(currentNum)
+        return fibArray
     }
 }
